73c17991e25b7389a8da50dc934c2b47b32a42de
Muhammed Emin Sönmez <123305942+xenerieza@users.noreply.github.com>
2023-05-22 00:21:02 +0300

    Update BMP180.h

--- BMP180_Driver/Library_File/BMP180.h
+++ BMP180_Driver/Library_File/BMP180.h
@@ -64,23 +64,6 @@ extern I2C_HandleTypeDef hi2c1;
 #define ctrl_meas	(0xF4)
 #define Soft_Reset	(0xE0)
 
-/**/
-/*	Calibration Structures	*/
-typedef struct Calibration_Values_Structure
-{
-	int16_t AC1;
-	int16_t AC2;
-	int16_t AC3;
-	uint16_t AC4;
-	uint16_t AC5;
-	uint16_t AC6;
-	int16_t B1;
-	int16_t B2;
-	int16_t MB;
-	int16_t MC;
-	int16_t MD;
-}Calibration_Values_Structure;
-
 /*	Temperature & Pressure Structures	*/
 typedef struct BMP180_Temperature_Parameters_Structure
 {
